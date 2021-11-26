0ed9e025b12e623d95fab5f6df57b7244c5645c9
Arthur Silva <arthurmatsilva@outlook.com>
2021-11-26 08:22:17 -0300

    making the box shows where user clicks and game image as an asset

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -1,8 +1,18 @@
 import './styles.css';
 
-export function Box() {
+export function Box({ show, screenX, screenY }) {
+
+  console.log(show, screenX, screenY);
+
   return (
-    <div id='box'>
+    <div
+      id='box'
+      style={{
+        display: show ? 'block' : 'none',
+        top: screenY,
+        left: screenX
+      }}
+    >
       <ul>
         <li>Waldo</li>
         <li>Wizard</li>
--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -1,25 +1,37 @@
+import { useState } from 'react';
+
 import { Box } from '../Box';
+
+import gameImage from '../../assets/where-s-waldo.jpg';
 
 import './styles.css';
 
 export function Picture() {
+  const [show, setShow] = useState(false);
+  const [screenX, setScreenX] = useState(0);
+  const [screenY, setScreenY] = useState(0);
   
   function handleClick(event) {
-    const { screenX, screenY } = event;
+    const { clientX, clientY } = event;
 
-    console.log(screenX, screenY);
-    // render box
+    setShow(!show);
+    setScreenX(clientX);
+    setScreenY(clientY);
   }
 
   return (
     <>
       <img
         id='picture'
-        src='https://i.redd.it/p9ye06summm51.jpg'
+        src={gameImage}
         alt='game screen'
         onClick={handleClick}
       />
-      <Box />
+      <Box
+        show={show}
+        screenX={screenX}
+        screenY={screenY}
+      />
     </>
   );
 }
